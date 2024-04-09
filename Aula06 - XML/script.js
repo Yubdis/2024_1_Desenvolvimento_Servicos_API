f8d95d26e6488686d84794fe94b53950146bdfd5
SIMON KAR SING TSUI <632310191@fspoa.br>
2024-04-09 12:05:48 -0300

    Aula06 - XML

---
+++ Aula06 - XML/script.js
@@ -0,0 +1,118 @@
+function lerDados(){
+    xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200){
+            dados = this.responseXML;
+            // console.log(dados);
+
+            pessoa = dados.getElementByTagName("pessoa");
+
+            nome = pessoa[0].getElementByTagName("nome");
+            nomePessoa = nome[0].childNodes[0].nodeValue;
+
+            idade = pessoa[0].getElementByTagName("idade");
+            idadePessoa = idade[0].childNodes[0].nodeValue;
+            
+            texto += "Nome: " + nomePessoa + "<br>";
+            texto += "Idade: " + idadePessoa + "<br>";
+            
+            document.getElementById("divDados").innerHTML = texto;
+        }
+    };
+
+
+    xhttp.open("GET", "dados.xml", true);
+    xhttp.send();
+}
+
+function lerProdutos(){
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200){
+            dados = this.responseXML;
+
+            // if (dados.getElementsByTagName("resposta") ) {
+            //     erro = dados.getElementsByTagName("resposta");
+            //     alert( erro );
+            // }else{
+                tabela = "<table border='1'>";
+                tabela += "     <tr>";
+                tabela += "         <th>Código</th>";
+                tabela += "         <th>Nome</th>";
+                tabela += "         <th>Preço</th>";
+                tabela += "         <th>Quantidade</th>";
+                tabela += "         <th>CodCategoria</th>";
+                tabela += "         <th>Excluir</th>";
+                tabela += "     </tr>";
+                produtos = dados.getElementsByTagName("produto");
+
+                for (i=0; i < produtos.length; i++){
+                
+                    tabela += " <tr>";
+                    id = produtos[i].getElementsByTagName("id")[0].childNodes[0].nodeValue;
+                    tabela += "     <td>"+id+"</td>";
+
+                    nome = produtos[i].getElementsByTagName("nome")[0].childNodes[0].nodeValue;
+                    tabela += "     <td>"+nome+"</td>";
+
+                    preco = produtos[i].getElementsByTagName("preco")[0].childNodes[0].nodeValue;
+                    tabela += "     <td>"+preco+"</td>";
+
+                    qtd = produtos[i].getElementsByTagName("qtd")[0].childNodes[0].nodeValue;
+                    tabela += "     <td>"+qtd+"</td>";
+
+                    cat = produtos[i].getElementsByTagName("categoria")[0].childNodes[0].nodeValue;
+                    tabela += "     <td>"+cat+"</td>";
+
+                    tabela += "     <td><button onclick='excluir("+id+")'>Excluir</button></td>";
+                    tabela += " </tr>";
+                
+                } ;
+                tabela += "</table>";
+                document.getElementById("divProdutos").innerHTML = tabela;
+            }
+            
+
+        // }
+    };
+    xhttp.open("GET", "servidor.php?buscar", true);
+    xhttp.send();
+}
+
+function excluir(id){
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200){
+            dados = JSON.parse(this.responseText);
+            if( dados.resposta){
+                alert( dados.resposta );
+                lerProdutos();
+            }
+        }
+
+    }
+    xhttp.open("GET", "servidor.php?excluir&id="+id, true);
+    xhttp.send();
+}
+
+function inserir(){
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200){
+            dados = JSON.parse(this.responseText);
+            if( dados.resposta){
+                alert( dados.resposta );
+                lerProdutos();
+            }
+        }
+
+    }
+    nome = document.getElementById("txtNome").value;
+    preco = document.getElementById("txtPreco").value;
+    qtd = document.getElementById("txtQtd").value;
+    xhttp.open("POST", "servidor.php?inserir", true);
+    xhttp.setRequestHeader("Content-type",
+            "application/x-www-form-urlencoded");
+    xhttp.send("nome="+nome+"&preco="+preco+"&qtd="+qtd);
+}
---
+++ Aula06 - XML/servidor.php
@@ -0,0 +1,78 @@
+<?php
+    
+    header("Content-type: application/xml; charset='UTF-8' ");
+
+    $xml = '<?xml version="1.0" encoding="UTF-8" ?>';
+
+    if( isset($_REQUEST["buscar"]) ){
+        try{
+            $conn = mysqli_connect("localhost", "root", "", "loja");
+            if ($conn){
+                $result = mysqli_query($conn, "SELECT * FROM produto");
+                $xml .= "<lista_produtos>";
+                while( $prod = mysqli_fetch_array($result) ){
+                    $xml .= '<produto>';
+                    $xml .= '   <id>'.$prod["id"].'</id>';
+                    $xml .= '   <nome>'.$prod["nome"].'</nome>';
+                    $xml .= '   <preco>'.$prod["preco"].'</preco>';
+                    $xml .= '   <qtd>'.$prod["quantidade"].'</qtd>';
+                    $xml .= '   <categoria>'.$prod["codCategoria"].'</categoria>';
+                    $xml .= '</produto>';
+                }
+                $xml .= "</lista_produtos>";
+                mysqli_close($conn);
+    
+                echo $xml;
+            }else{
+                $xml .= '<resposta>Erro ao connectar com o Banco de dados</resposta>';
+                echo $xml;
+            }
+        }catch(\Throwable $th){
+            $xml .= '<resposta>Erro ao consultar o Banco de dados</resposta>';
+            echo $xml;
+        }
+
+    }
+
+    if( isset($_REQUEST["excluir"]) ){
+        try{
+            $conn = mysqli_connect("localhost", "root", "", "loja");
+            if ($conn){
+                $id = $_GET["id"];
+                $result = mysqli_query($conn, "DELETE FROM produto WHERE id = $id");
+                mysqli_close($conn);
+                echo '{ "resposta" : "Produto Excluido com sucesso"}';
+            }else{
+                echo '{ "resposta" : "Erro ao connectar com o Banco de dados"}';
+            }
+        }catch(\Throwable $th){
+            echo '{ "resposta" : "Erro ao consultar o Banco de dados"}';
+        }
+
+    }
+
+    if( isset($_REQUEST["inserir"]) ){
+        try{
+            $conn = mysqli_connect("localhost", "root", "", "loja");
+            if ($conn){
+                $nome = $_POST["nome"];
+                $preco = $_POST["preco"];
+                $qtd = $_POST["qtd"];
+                $sql = "INSERT INTO produto (nome, preco, quantidade) VALUES
+                        ('$nome', $preco, $qtd ) ";
+                $result = mysqli_query($conn, $sql);
+                mysqli_close($conn);
+                if ($result > 0){    
+                    echo '{ "resposta" : "Produto inserido com sucesso"}';
+                }else{
+                    echo '{ "resposta" : "Erro ao connectar com o Banco de dados"}';
+                }
+            }
+        }catch(\Throwable $th){
+            echo '{ "resposta" : "Erro ao consultar o Banco de dados"}';
+        }
+
+    }
+
+
+?>
